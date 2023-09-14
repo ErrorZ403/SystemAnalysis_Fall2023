a0a5610c508872c92d4160cf605bc5f09f3e24a7
unknown <Nikolay>
2023-09-14 11:48:59 +0300

    Update task.py
    Added function for reading csv file and returning value in particular postion

--- task1/task.py
+++ task1/task.py
@@ -1,1 +1,31 @@
 #Task 1
+
+import csv
+import argparse
+
+def read_csv(file_path, row, column):
+    counter = 0
+    with open(file_path, 'r') as table:
+        csvreader = csv.reader(table)
+        for rowt in csvreader:
+            if counter == row:
+                try:
+                    return rowt[column]
+                except:
+                    raise Exception('index out of range')
+            
+            counter += 1
+
+    return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('filepath')
+    parser.add_argument('-r', '--row', type=int)
+    parser.add_argument('-c', '--column', type=int)
+
+    args = parser.parse_args()
+    answer = read_csv(args.filepath, args.row, args.column)
+
+    print(answer)
